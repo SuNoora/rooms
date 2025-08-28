1b80dc377e70dd343aa3bebe308d60cc93dd9dc0
SuNoora <snurzadas02@gmail.com>
2025-08-28 23:08:06 +0600

    test commit

--- src/main/java/kg/megalab/rooms/mappers/RoomMapper.java
+++ src/main/java/kg/megalab/rooms/mappers/RoomMapper.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface RoomMapper {
 
     RoomMapper INSTANSE = Mappers.getMapper(RoomMapper.class);
-
+    // test comment
     @Mapping(source = "id", target = "roomId")
     RoomDto roomToRoomDto(Room room);
 
